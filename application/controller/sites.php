5efcb2908d9d14434868734133a37561bfee83e5
Dave Bray <david@brayworth.com.au>
2018-02-26 10:04:27 +1000

    checked security

--- application/controller/sites.php
+++ application/controller/sites.php
@@ -1,5 +1,5 @@
 <?php
-/**
+/*
 	David Bray
 	BrayWorth Pty Ltd
 	e. david@brayworth.com.au
@@ -17,28 +17,38 @@
 class sites extends Controller {
 
   protected function _index() {
-    $dao = new dao\sites;
-    $this->data = (object)[
-      'sites' => $dao->getAll()
+    if ( currentUser::isAdmin()) {
+      $dao = new dao\sites;
+      $this->data = (object)[
+        'sites' => $dao->getAll()
 
-    ];
+      ];
 
-    $p = new page( $this->title = 'Sites');
-			$p
-				->header()
-				->title();
+      $p = new page( $this->title = 'Sites');
+  			$p
+  				->header()
+  				->title();
 
-			$p->primary();
-				$this->load('list');
+  			$p->primary();
+  				$this->load('list');
 
-			$p->secondary();
-				//~ $this->load('index');
-				$this->load('main-index');
+  			$p->secondary();
+  				//~ $this->load('index');
+  				$this->load('main-index');
+
+    }
 
   }
 
   public function index() {
-    $this->_index();
+    if ( currentUser::isAdmin()) {
+      $this->_index();
+
+    }
+    else {
+      Response::redirect( url::tostring());
+
+    }
 
   }
 
