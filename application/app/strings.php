556da0540c2b5dabd01fd6f32117470b1336d066
Dave Bray <david@brayworth.com.au>
2018-02-27 22:18:00 +1000

    formatting

--- application/app/strings.php
+++ application/app/strings.php
@@ -10,18 +10,59 @@
 
 abstract class strings extends dvc\strings {
   static function getGUID(){
-    mt_srand((double)microtime()*10000);//optional for php 4.2.0 and up.
+    mt_srand( (double)microtime() * 10000);  //optional for php 4.2.0 and up.
     $charid = strtoupper(md5(uniqid(rand(), true)));
-    $hyphen = chr(45);// "-"
-    $uuid = chr(123)// "{"
+    $hyphen = chr(45);  // "-"
+    $uuid = chr(123)    // "{"
       .substr($charid, 0, 8).$hyphen
       .substr($charid, 8, 4).$hyphen
       .substr($charid,12, 4).$hyphen
       .substr($charid,16, 4).$hyphen
       .substr($charid,20,12)
-      .chr(125);// "}"
-      return $uuid;
+      .chr(125);        // "}"
+
+    return $uuid;
+
+  }
+
+  static function ShortLicense( $license) {
+    if ( 'easydoseOPEN' == $license)
+      return 'OPEN';
+
+    return ( $license);
+
+  }
+
+  static function StringToOS( $osIn) {
+    if ( preg_match( '@^Microsoft Windows XP@', $osIn )) {
+      return 'WinXP';
 
     }
+    elseif ( preg_match( '@^Microsoft Windows \[Version 6.1.7601\]@', $osIn )) {
+      return 'Win7/2008 SP1';
+
+    }
+    elseif ( preg_match( '@^Microsoft Windows \[Version 6.2.9200\]@', $osIn )) {
+      return 'Win8/2012';
+
+    }
+    elseif ( preg_match( '@^Microsoft Windows \[Version 6.3.9200\]@', $osIn )) {
+      return 'Win8.1/2012 R2';
+
+    }
+    elseif ( preg_match( '@^Microsoft Windows \[Version 6.3.9600\]@', $osIn )) {
+      return 'Win8.1 U1/2012';
+
+    }
+    elseif ( preg_match( '@^Microsoft Windows \[Version 10@', $osIn )) {
+      return 'Win10';
+
+    }
+    else {
+      return $osIn;
+
+    }
+
+  }
 
 }
--- application/app/views/sites/list.php
+++ application/app/views/sites/list.php
@@ -8,97 +8,83 @@
 		http://creativecommons.org/licenses/by/4.0/
 
 	*/ ?>
-<table class="table table-striped table-sm small">
-  <colgroup>
-    <col style="width: 70px;" />
-    <col />
-    <col style="width: 90px;" />
-    <col span="4" />
-    <col style="width: 100px;" />
-    <col />
-    <col />
-    <col />
-    <col style="width: 80px;" />
-    <col style="width: 90px;" />
-  </colgroup>
+<div class="row">
+  <div class="col p-0">
+    <table class="table table-striped table-sm small">
+      <thead>
+        <tr>
+          <td>State</td>
+          <td role="sort-header" data-key="site">Site</td>
+          <td>Tel.</td>
+          <td class="d-none d-lg-table-cell">IP</td>
+          <td class="d-none d-lg-table-cell">Workstation</td>
+          <td>Product</td>
+          <td>Active/<br />Patients</td>
+          <td class="d-none d-lg-table-cell">OS</td>
+          <td class="d-none d-lg-table-cell">Deploy</td>
+          <td class="d-none d-lg-table-cell" role="sort-header" data-key="version">Version</td>
+          <td>Act</td>
+          <td class="d-none d-md-table-cell" role="sort-header" data-key="expires">Expires</td>
+          <td class="d-none d-lg-table-cell">Update</td>
 
-  <thead>
-    <tr>
-      <td>State</td>
-      <td role="sort-header" data-key="site">Site</td>
-      <td>Tel.</td>
-      <td>IP</td>
-      <td>Workstation</td>
-      <td>Product ID</td>
-      <td>Active/<br />Patients</td>
-      <td>OS</td>
-      <td>Deploy</td>
-      <td role="sort-header" data-key="version">Version</td>
-      <td>Active</td>
-      <td role="sort-header" data-key="expires">Expires</td>
-      <td>Updated</td>
+        </tr>
 
-    </tr>
+      </thead>
 
-  </thead>
-
-  <tbody>
+      <tbody>
 <?php while ( $dto = $this->data->sites->dto()) { ?>
-      <tr
-        data-site="<?php print $dto->site ?>"
-        data-version="<?php print $dto->version ?>"
-        data-expires="<?php print $dto->expires ?>">
+          <tr
+            data-site="<?php print $dto->site ?>"
+            data-version="<?php print $dto->version ?>"
+            data-expires="<?php print $dto->expires ?>">
 
-        <td><?php print $dto->state ?></td>
-        <td><?php print $dto->site ?></td>
-        <td><?php print $dto->tel ?></td>
-        <td><?php print $dto->ip ?></td>
-        <td><?php print $dto->workstation ?></td>
-        <td><?php print $dto->productid ?></td>
-        <td><?php print sprintf( '%s/%s', $dto->patients, $dto->patientsActive) ?></td>
+            <td><?php print $dto->state ?></td>
+            <td><?php print $dto->site ?></td>
+            <td><?php print $dto->tel ?></td>
+            <td class="d-none d-lg-table-cell"><?php print $dto->ip ?></td>
+            <td class="d-none d-lg-table-cell"><?php print $dto->workstation ?></td>
+            <td><?php print strings::ShortLicense( $dto->productid); ?></td>
+            <td><?php print sprintf( '%s/%s', $dto->patients, $dto->patientsActive) ?></td>
+            <td class="d-none d-lg-table-cell"><?php print strings::StringToOS($dto->os) ?></td>
+            <td class="d-none d-lg-table-cell"><?php print $dto->deployment ?></td>
+            <td class="d-none d-lg-table-cell"><?php print $dto->version ?></td>
+            <td><?php print ( $dto->activated ? 'yes' : 'no') ?></td>
+            <td class="d-none d-md-table-cell"><?php print date( \config::$DATE_FORMAT, strtotime( $dto->expires )) ?></td>
+            <td class="d-none d-lg-table-cell"><?php print date( "d/m h:m", strtotime( $dto->updated)) ?></td>
 
-        <td><?php
-    if ( preg_match( '@^Microsoft Windows XP@', $dto->os ))
-      print 'WinXP';
-
-    elseif ( preg_match( '@^Microsoft Windows \[Version 6.1.7601\]@', $dto->os ))
-      print 'Win7/2008 SP1';
-
-    elseif ( preg_match( '@^Microsoft Windows \[Version 6.2.9200\]@', $dto->os ))
-      print 'Win8/2012';
-
-    elseif ( preg_match( '@^Microsoft Windows \[Version 6.3.9200\]@', $dto->os ))
-      print 'Win8.1/2012 R2';
-
-    elseif ( preg_match( '@^Microsoft Windows \[Version 6.3.9600\]@', $dto->os ))
-      print 'Win8.1 U1/2012';
-
-    elseif ( preg_match( '@^Microsoft Windows \[Version 10@', $dto->os ))
-      print 'Win10';
-
-    else
-      print $dto->os;
-
-        ?></td>
-
-        <td><?php print $dto->deployment ?></td>
-        <td><?php print $dto->version ?></td>
-        <td><?php print ( $dto->activated ? 'yes' : 'no') ?></td>
-        <td><?php print date( \config::$DATE_FORMAT, strtotime( $dto->expires )) ?></td>
-        <td><?php print date( "d/m h:m", strtotime( $dto->updated)) ?></td>
-
-      </tr>
+          </tr>
 
 <?php } // while ( $dto = $this->data->sites->dto()) ?>
 
-  </tbody>
+      </tbody>
 
-  <tfoot>
-    <tr>
-      <td colspan="13"><em><?php
-        print date( 'c');
-      ?></em></td>
-    </tr>
-  </tfoot>
+      <tfoot>
+        <tr>
+          <td colspan="13" class="p-0">
+            <div class="container-fluid">
+              <div class="row">
+                <div class="col">
+                  <em><?php printf( 'count: %s', count( $this->data->sites)); ?></em>
 
-</table>
+                </div>
+
+                <div class="col text-right">
+                  <em><?php print date( 'c'); ?></em>
+
+                </div>
+
+              </div>
+
+            </div>
+
+          </td>
+
+        </tr>
+
+      </tfoot>
+
+    </table>
+
+  </div>
+
+</div>
