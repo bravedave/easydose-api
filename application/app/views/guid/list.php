d728c0b28fe9c1eb63386ce5420e2152dd24a45d
Dave Bray <david@brayworth.com.au>
2018-08-09 09:46:13 +1000

    added indicator license is in use added ability to change license in use status from context

--- application/app/views/guid/list.php
+++ application/app/views/guid/list.php
@@ -15,6 +15,7 @@
           <td class="d-none d-lg-table-cell" style="width: 40px;">id</td>
           <td>guid</td>
           <td class="d-none d-lg-table-cell" style="width: 18em;">name</td>
+          <td class="text-center" style="width: 5em;">use license</td>
           <td class="text-center" style="width: 8em;">created</td>
           <td class="d-none d-lg-table-cell text-center" style="width: 8em;">updated</td>
 
@@ -25,7 +26,9 @@
       <tbody>
         <?php while ( $dto = $this->data->res->dto()) {  ?>
           <tr
-            data-id="<?php print $dto->id ?>" row-guid>
+            data-id="<?php print $dto->id ?>"
+            data-license="<?php print (int)$dto->use_license ?>"
+            row-guid>
             <td class="d-none d-lg-table-cell"><?php print $dto->id ?></td>
             <td><?php print $dto->guid ?>
               <div class="text-muted small">
@@ -35,6 +38,10 @@
 
             </td>
             <td class="d-none d-lg-table-cell"><?php print $dto->name ?></td>
+            <td class="text-center" style="width: 5em;" license-indicator><?php
+              if ( $dto->use_license ) {
+                print strings::html_tick;
+              } ?></td>
             <td class="text-center"><?php print date( \config::$DATE_FORMAT, strtotime( $dto->created)) ?></td>
             <td class="d-none d-lg-table-cell text-center"><?php print date( \config::$DATE_FORMAT, strtotime( $dto->updated)) ?></td>
 
@@ -70,8 +77,45 @@ $(document).ready( function() {
 
       e.stopPropagation(); e.preventDefault();
 
-      var _context = _brayworth_.context();
-      _context.append($('<a><i class="fa fa-link"></i>view</a>').attr('href',_brayworth_.url('guid/view/'+id)))
+      let _context = _brayworth_.context();
+      _context.append( $('<a><i class="fa fa-link"></i>view</a>').attr('href',_brayworth_.url('guid/view/'+id)));
+      _context.append( ( function() {
+        let a = $('<a href="#">use this license</a>').on('click', function(e) {
+          e.stopPropagation(); e.preventDefault();
+
+          let newVal = ( _tr.data('license') == 1 ? 0 : 1)
+
+          _brayworth_.post({
+            url : _brayworth_.url('guid'),
+            data : {
+              action : 'use-version-2-license',
+              value : newVal,
+              id : id,
+
+            }
+
+          })
+          .then( function(d) {
+            _brayworth_.growl(d);
+            _tr.data('license', newVal)
+            $('td[license-indicator]', _tr).html(newVal == 1 ? '<?php print strings::html_tick ?>' : '');
+
+          });
+
+          _context.close();
+
+        });
+
+        if ( _tr.data('license') == 1) {
+            a.prepend('<i class="fa fa-check"></i>');
+            
+        }
+
+        return (a);
+
+      })());
+
+      _context.append('<hr />');
       _context.append($('<a href="#"><i class="fa fa-trash"></i>delete</a>').on('click', function(e) {
         e.stopPropagation(); e.preventDefault();
 
