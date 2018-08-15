e59b6508202199f19f1d240422e9a58ee01020e8
Dave Bray <david@brayworth.com.au>
2018-08-15 12:54:31 +1000

    reduce padding

--- application/app/views/navbar-default.php
+++ application/app/views/navbar-default.php
@@ -10,7 +10,7 @@
 	DO NOT change this file
 	Copy it to <application>/app/views/ and modify it there
 	*/	?>
-	<nav class="navbar navbar-expand-md navbar-dark bg-primary sticky-top d-print-none" role="navigation" >
+	<nav class="navbar navbar-expand-md navbar-dark bg-primary sticky-top d-print-none py-0" role="navigation" >
 		<div class="navbar-brand">
 			<?php print $this->data->title ?>
 
@@ -27,7 +27,7 @@
  		</button>
 
 		<div class="collapse navbar-collapse" id="navbarToggler">
-			<ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+			<ul class="navbar-nav mr-auto">
 				<li class="nav-item">
 					<?php printf( '<a href="%s" class="nav-link"><i class="fa fa-fw fa-home"></i>Home</a>', \url::$URL);	?>
 
@@ -36,7 +36,7 @@
 				<li class="nav-item"><a class="nav-link" href="<?php url::write('account'); ?>">My Account</a></li>
 				<?php if ( \sys::lockdown()) {	?>
 					<li class="nav-item"><a class="nav-link" href="<?php url::write('logout') ?>">Logout</a></li>
-					
+
 				<?php } // if ( \sys::lockdown())	?>
 
 			</ul>
