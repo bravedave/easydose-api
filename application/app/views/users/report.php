6a3a27cf3f4c93f870451758683d28f7af687397
Dave Bray <david@brayworth.com.au>
2018-08-31 06:13:43 +1000

    Added download as CSV * covers off on emails for bulk mail - download as CSV and copy the email column * changed Name header to Proprietor * Search field * Sort on all columns

--- application/app/views/users/report.php
+++ application/app/views/users/report.php
@@ -8,45 +8,97 @@
 		http://creativecommons.org/licenses/by/4.0/
 
 	*/	?>
-<table class="table table-striped">
-	<thead>
-		<tr>
-			<td>Name</td>
-			<td>UserName</td>
-			<td>Email</td>
-			<td class="text-center">Admin</td>
-		</tr>
-	</thead>
+<style>
+i[title="download as CSV"] {margin-top: -18px;}
+</style>
+<div class="row">
+  <div class="col-md-6">
+      <input type="search" name="<?php print $sid = uniqid( 'ed') ?>"
+        id="<?php print $sid ?>" placeholder="search..." class="form-control"
+        autofocus />
 
-	<tbody>
-<?php	if ( $this->data) {
-			while ( $dto = $this->data->dto()) {	?>
-		<tr data-role="item" data-id="<?php print $dto->id ?>">
-			<td><?php
-			 	print $dto->name;
-				printf( '<div class="text-muted small">%s</div>', $dto->site );
-			?></td>
-			<td>
-				<div class="text-truncate" style="width: 12em">
-					<?php print $dto->username ?>
+  </div>
 
-				</div>
+</div>
 
-			</td>
-			<td><?php print $dto->email ?></td>
-			<td class="text-center"><?php print ( $dto->admin ? strings::html_tick : '&nbsp;' ) ?></td>
+<div class="row">
+	<div class="col">
+		<table class="table table-sm" id="<?php print $tblID = uniqid( 'ed') ?>">
+			<thead>
+				<tr>
+					<td role="sort-header" data-key="name">Proprietor</td>
+					<td role="sort-header" data-key="username">UserName</td>
+					<td role="sort-header" data-key="email">Email</td>
+					<td class="text-center" role="sort-header" data-key="admin" data-sorttype="numeric">Admin</td>
+				</tr>
+			</thead>
 
-		</tr>
+			<tbody>
+				<?php	if ( $this->data) {
+					while ( $dto = $this->data->dto()) {	?>
+						<tr data-role="item"
+							data-id="<?php print $dto->id ?>"
+							data-name="<?php print $dto->name ?>"
+							data-username="<?php print $dto->username ?>"
+							data-email="<?php print $dto->email ?>"
+							data-admin="<?php print (int)$dto->admin ?>"
+							>
+							<td><?php
+							print $dto->name;
+							printf( '<div class="text-muted small">%s</div>', $dto->site );
+							?></td>
+							<td>
+								<div class="text-truncate" style="width: 12rem">
+									<?php print $dto->username ?>
 
-<?php		}
+								</div>
 
-		}	?>
+							</td>
+							<td><?php print $dto->email ?></td>
+							<td class="text-center"><?php print ( $dto->admin ? strings::html_tick : '&nbsp;' ) ?></td>
 
-	</tbody>
+						</tr>
 
-</table>
+					<?php		}
+
+				}	?>
+
+			</tbody>
+
+		</table>
+
+	</div>
+
+</div>
 <script>
 $(document).ready( function() {
+	$('#<?php print $sid ?>').on( 'keyup', function(e) {
+		var _me = $(this);
+		var t = _me.val();
+
+		$('tr[data-role="item"]').each( function( i, tr) {
+			var _tr = $(tr);
+
+			if ( t == '') {
+				_tr.removeClass('d-none');
+			}
+			else {
+				var rex = new RegExp(t,'i')
+				// console.log( t, _tr.text())
+				if ( rex.test( _tr.text())) {
+					_tr.removeClass('d-none');
+				}
+				else {
+					_tr.addClass('d-none');
+
+				}
+
+			}
+
+		});
+
+	});
+
 	$('tr[data-role="item"]').each( function( i, el) {
 		var _el = $(el);
 		var editURL = _brayworth_.url('users/view/' + _el.data('id'));
@@ -107,12 +159,24 @@ $(document).ready( function() {
 
 			}));
 
-
 			_context.open( e);
 
 		});
 
 	});
+
+	/*--[ a CSV download icon ]--*/
+  (function( tbl) {
+		if ( tbl.length == 1) {
+			$('<i class="fa fa-fw fa-table noprint pointer pull-right" title="download as CSV" />')
+			.on( 'click', function( e) {
+				_ed_.csv.call( tbl, 'users-list.csv');
+			})
+			.insertBefore( tbl);
+
+		}
+
+	})( $("#<?php print $tblID ?>"));
 
 })
 </script>
