9d9be4d82121ecf56976cb914527ee4fc853a09c
Dave Bray <david@brayworth.com.au>
2020-05-04 14:24:59 +1000

    fix autloader

--- application/autoloader.php
+++ application/autoloader.php
@@ -1,12 +1,20 @@
 <?php
 /*
-	David Bray
-	BrayWorth Pty Ltd
-	e. david@brayworth.com.au
+ * David Bray
+ * BrayWorth Pty Ltd
+ * e. david@brayworth.com.au
+ *
+ * MIT License
+ *
+*/
 
-	This work is licensed under a Creative Commons Attribution 4.0 International Public License.
-		http://creativecommons.org/licenses/by/4.0/
-	*/
+$autoload = __DIR__ . '/../vendor/bravedave/dvc/autoloader.php';
+
+if ( file_exists( $autoload))
+	require_once $autoload;
+
+else
+	throw new Exception( 'Unable to locate autoloader');
 
 spl_autoload_register(function ($class) {
 	if ( $lib = realpath( __DIR__ . '/app/' . str_replace('\\', '/', $class) . '.php')) {
@@ -19,22 +27,5 @@ spl_autoload_register(function ($class) {
 
 	return ( false);
 
-});
+}, true, true);
 
-$autoload = __DIR__ . '/../vendor/bravedave/dvc/autoloader.php';
-$autoloadLocal = __DIR__ . '/../../dvc/autoloader-local.php';
-//~ print __DIR__ . '<br />';
-//~ print realpath( __DIR__) . '<br />';
-//~ print realpath( __DIR__ . '/../') . '<br />';
-//~ print realpath( __DIR__ . '/../../') . '<br />';
-//~ print realpath( __DIR__ . '/../../dvc/autoloader-local.php') . '<br />';
-//~ die;
-
-if ( file_exists( $autoload))
-	require_once $autoload;
-
-elseif ( file_exists( $autoloadLocal))
-	require_once $autoloadLocal;
-
-else
-	throw new Exception( 'Unable to locate autoloader');
