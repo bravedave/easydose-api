a8f346976e82cb3f5e28d7bc5cfcefd098650b28
Dave Bray <david@brayworth.com.au>
2021-12-18 12:13:42 +1000

    runtime

--- run.sh
+++ run.sh
@@ -2,8 +2,11 @@
 
 WD=`pwd`
 PORT=$[RANDOM%1000+1024]
+PORT=8057
+php=php
+if [[ -x /usr/bin/php8 ]]; then php=php8; fi
 
 cd www
 echo "this application is available at http://localhost:$PORT"
-php -S localhost:$PORT _dvc.php
+$php -S localhost:$PORT _dvc.php
 cd $WD
